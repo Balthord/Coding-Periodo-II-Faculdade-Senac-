05ffbc38520462643650b4fd86f2ff57c877e63a
Lucas do Nascimento <lucasdoncr07@hotmail.com>
2022-11-03 19:39:10 -0300

    commit 02
    ArrayList - métodos adicionar, mostrar e remover

--- Exercicio01_Java/Entidades/ConjuntoProdutos.java
+++ Exercicio01_Java/Entidades/ConjuntoProdutos.java
@@ -1,6 +1,32 @@
 package Entidades;
 import java.util.ArrayList;
 
-public class ConjuntoProdutos {
+public class ConjuntoProdutos { 
+    public static void main (String[] args){
+        ArrayList<Produto> Conjunto_Produtos = new ArrayList <>();
+        
+        Fornecedor forn1 = new Fornecedor ("1234321","978787887","Zé Maria");
+
+        Produto produto1 = new Produto (1,"TV LCD",3500,forn1);
+ 
+        Fornecedor forn2 = new Fornecedor ("543212345","8132314545","Cláudio");
+ 
+        Produto produto2 = new Produto (2,"notebook",2000, forn2);
+ 
+        Produto produto3 = new Produto (3,"impressora",232, forn2); 
+        
+        Conjunto_Produtos.add(produto1);
+        Conjunto_Produtos.add(produto2);
+        Conjunto_Produtos.add(produto3);
+
+        for (Produto objetoConjunto : Conjunto_Produtos) {
+            System.out.println("Nome:  "+ objetoConjunto.getNome() + "  Valor: " +objetoConjunto.getValor());
+        }
+        
+        Conjunto_Produtos.remove(produto3);
     
+        
+
+    }
 }
+
--- Exercicio01_Java/Entidades/Produto.java
+++ Exercicio01_Java/Entidades/Produto.java
@@ -5,8 +5,6 @@ public class Produto {
     private String nome;
     private float valor;
     private Fornecedor fornecedor;
-    
-    
     public Produto() {
     }
 
